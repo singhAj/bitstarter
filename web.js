15f945e924083433b55dd282aee469a131fede17
Ajay Singh <mailofajays@gmail.com>
2013-07-19 14:18:25 +0000

    web.js restored to old

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 var express = require('express');
 
 var app = express.createServer(express.logger());
@@ -11,7 +12,7 @@ app.get('/', function(request, response) {
 
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
